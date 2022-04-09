8b839cb8c1144aa1db8d1f1fe3fd35e20d8b6c44
Isaac Luton <isaacdluton@gmail.com>
2022-04-09 12:36:34 -0700

    adding quote interval functionality and additional javascript page

--- script.js
+++ script.js
@@ -1,43 +1,23 @@
-const musicEL = document.querySelector(".musicContainer")
-const imageEL = document.querySelector(".imageContainer")
-const quoteEL = document.querySelector(".quoteContainer")
-
-
-// imageApi = 'https://api.unsplash.com/photos/?client_id=R_iTdb6HaJJNCC32vNG2rbg9Am-stogGSupk5Xq9ZGs'
-
-quoteApi = "https://type.fit/api/quotes"
-
-
-// https://type.fit/api/quotes free code camp returns 1600 quotes
+const startBtn = document.querySelector("#startBtn")
+const timeInputVal = document.getElementById("timeInput").value
 
 
 
-// fetch(imageApi)
-// .then(function (response) {
-//   return response.json();
-// })
-// .then(function (imageData) {
-//   console.log(imageData)
 
-//   imageEL.textContent = 
+function redirect () {
+    
+  
+  window.location.href = "file:///Users/isaacluton/Documents/BootCamp/homework/zenTen/start.html"
 
-// });
+  let timeInputVal = document.getElementById("timeInput").value
 
-fetch(quoteApi)
-.then(function (response) {
-  return response.json();
-})
-.then(function (quoteData) {
-  console.log(quoteData)
+  localStorage.setItem("timeInput", timeInputVal)
+}
 
+function startBtnListener () {
 
-});
+  startBtn.addEventListener("click", redirect);
 
-// fetch(musicApi)
-// .then(function (response) {
-//   return response.json();
-// })
-// .then(function (musicData) {
-//   console.log(musicData)
+}
 
-// });
+startBtnListener ();
