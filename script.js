634d287fbe3634602f0fd90c8719b7a540d1d2ea
Julia Davis <juliakhdavis@gmail.com>
2022-04-09 20:04:05 -0700

    audio added

--- script.js
+++ script.js
@@ -1,104 +1,104 @@
-const musicContainer = document.querySelector('.music-container')
-const playBtn = document.querySelector('#play')
-const prevBtn = document.querySelector('#prev')
-const nextBtn = document.querySelector('#next')
-const audio = document.querySelector('#audio')
-const progress = document.querySelector('.progress')
-const progressContainer = document.querySelector('.progress-container')
-const title = document.querySelector('#title')
-const cover = document.querySelector('#cover')
+// const musicContainer = document.querySelector('.music-container')
+// const playBtn = document.querySelector('#play')
+// const prevBtn = document.querySelector('#prev')
+// const nextBtn = document.querySelector('#next')
+// const audio = document.querySelector('#audio')
+// const progress = document.querySelector('.progress')
+// const progressContainer = document.querySelector('.progress-container')
+// const title = document.querySelector('#title')
+// const cover = document.querySelector('#cover')
 
-// Enter song titles here as strings in an array
-const songs = ['songs', 'go', 'here']
+// // Enter song titles here as strings in an array
+// const songs = ['songs', 'go', 'here']
 
-// Keeps track of songs. index is two because we start count at zero
-let songIndex = 2
+// // Keeps track of songs. index is two because we start count at zero
+// let songIndex = 2
 
-// load songs into DOM
-loadSong(songs[songIndex])
+// // load songs into DOM
+// loadSong(songs[songIndex])
 
-function playSong() {
-    musicContainer.classList.add('play')
-    playBtn.querySelector('i.fas').classList.remove('fa-play')
-    playBtn.querySelector('i.fas').classList.add('fa-pause')
+// function playSong() {
+//     musicContainer.classList.add('play')
+//     playBtn.querySelector('i.fas').classList.remove('fa-play')
+//     playBtn.querySelector('i.fas').classList.add('fa-pause')
 
-    audio.play()
-}
+//     audio.play()
+// }
 
-function pauseSong() {
-    musicContainer.classList.add('play')
-    playBtn.querySelector('i.fas').classList.add('fa-play')
-    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+// function pauseSong() {
+//     musicContainer.classList.add('play')
+//     playBtn.querySelector('i.fas').classList.add('fa-play')
+//     playBtn.querySelector('i.fas').classList.remove('fa-pause')
 
-    audio.pause()
-}
+//     audio.pause()
+// }
 
-function prevSong() {
-    songIndex--
+// function prevSong() {
+//     songIndex--
 
-    if(songIndex < 0) {
-        songIndex= songs.length - 1
-    }
+//     if(songIndex < 0) {
+//         songIndex= songs.length - 1
+//     }
 
-    loadSong(songs[songIndex])
+//     loadSong(songs[songIndex])
 
-    playSong()
-}
+//     playSong()
+// }
 
-function nextSong() {
-    songIndex++
+// function nextSong() {
+//     songIndex++
 
-    if(songIndex > song.length - 1) {
-        songIndex = 0
-    }
+//     if(songIndex > song.length - 1) {
+//         songIndex = 0
+//     }
 
-    loadSong(songs[songIndex])
+//     loadSong(songs[songIndex])
 
-    playSong()
-}
+//     playSong()
+// }
 
-// update song details 
-function loadSong(song) {
-    title.innerText = song
-    audio.src = `music/${song}.mp3` //COME BACK AND CHECK SYNTAX LATER
-    cover.src = `images/${song}.jpg`
-}
+// // update song details 
+// function loadSong(song) {
+//     title.innerText = song
+//     audio.src = `music/${song}.mp3` //COME BACK AND CHECK SYNTAX LATER
+//     cover.src = `images/${song}.jpg`
+// }
 
-function updateProgress(e) {
-    // console.log(e.srcElement.duration)
-    const {duration, currentTime} = e.srcElement
-    const progressPercent = (currentTime / duration) * 100
-    progress.getElementsByClassName.width = `${progressPercent}%` //COME BACK TO THIS
-}
+// function updateProgress(e) {
+//     // console.log(e.srcElement.duration)
+//     const {duration, currentTime} = e.srcElement
+//     const progressPercent = (currentTime / duration) * 100
+//     progress.getElementsByClassName.width = `${progressPercent}%` //COME BACK TO THIS
+// }
 
-function setProgress(e) {
-    const width = this.clientWidth // MUSIC PROGRESS BAR LENGTH
-    const clickX = e.offsetX // COME BACK TO THIS
-    const duration = audio.duration
+// function setProgress(e) {
+//     const width = this.clientWidth // MUSIC PROGRESS BAR LENGTH
+//     const clickX = e.offsetX // COME BACK TO THIS
+//     const duration = audio.duration
     
-}
+// }
 
-// Event listeners
-playBtn.addEventListener('click', () => {
-    const isPLaying = musicContainer.classList.contains('play')
+// // Event listeners
+// playBtn.addEventListener('click', () => {
+//     const isPLaying = musicContainer.classList.contains('play')
 
-    if(isPlaying) {
-        pauseSong()
-    } else {
-        playSong()
-    }
-})
+//     if(isPlaying) {
+//         pauseSong()
+//     } else {
+//         playSong()
+//     }
+// })
 
-// Change song events
-prevBtn.addEventListener('click', prevSong)
-prevBtn.addEventListener('click', nextSong)
+// // Change song events
+// prevBtn.addEventListener('click', prevSong)
+// prevBtn.addEventListener('click', nextSong)
 
-audio.addEventListener('timeupdate', updateProgress)
+// audio.addEventListener('timeupdate', updateProgress)
 
-progressContainer.addEventListener('click', setProgress)
-const musicEL = document.querySelector(".musicContainer")
-const imageEL = document.querySelector(".imageContainer")
-const quoteEL = document.querySelector(".quoteContainer")
+// progressContainer.addEventListener('click', setProgress)
+// const musicEL = document.querySelector(".musicContainer")
+// const imageEL = document.querySelector(".imageContainer")
+// const quoteEL = document.querySelector(".quoteContainer")
 
 
 // imageApi = 'https://api.unsplash.com/photos/?client_id=R_iTdb6HaJJNCC32vNG2rbg9Am-stogGSupk5Xq9ZGs'
